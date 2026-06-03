76f5f762c9ef6ad52e677eda85b9f4780d5b3820
Catalin <cata.bota1998@yahoo.com>
2026-06-03 18:35:38 +0300

    Fix Chatbox import casing for Vercel

--- src/MoviesPage.jsx
+++ src/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import ChatBox from "./ChatBox";
+import ChatBox from "./Chatbox";
 import AdminPanel from "./AdminPanel";
 
 const emptyForm = {
